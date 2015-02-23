ba63b6c663b6c70738e1ad6f19c24ca23d5193b1
Raúl Pedraza <raulpedrazaleon@gmail.com>
2015-02-23 14:07:50 +0100

    Initial Commit

---
+++ MTBProyect/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MTBProyect
+//
+//  Created by Raul Pedraza on 23/2/15.
+//  Copyright (c) 2015 Raul Pedraza. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MTBProyect/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MTBProyect
+//
+//  Created by Raul Pedraza on 23/2/15.
+//  Copyright (c) 2015 Raul Pedraza. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
